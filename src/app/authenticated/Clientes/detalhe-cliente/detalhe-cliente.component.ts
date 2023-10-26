13e57625e57a4bbc71714fa28bf9accef9b96b4e
Filipe Noronha <98480552+filipelndev@users.noreply.github.com>
2023-10-25 21:01:54 -0300

    edit detalhe-cliente
    Alteração no detalhe-cliente para visualização e edição do mesmo.

--- src/app/authenticated/Clientes/detalhe-cliente/detalhe-cliente.component.ts
+++ src/app/authenticated/Clientes/detalhe-cliente/detalhe-cliente.component.ts
@@ -1,5 +1,7 @@
-import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
@@ -7,9 +9,42 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   templateUrl: './detalhe-cliente.component.html',
   styleUrls: ['./detalhe-cliente.component.css']
 })
-export class DetalheClienteComponent {
+export class DetalheClienteComponent implements OnInit {
+
+  DataCadastro: string | null = ''
+  UltimaAlteracao: string | null = '';
+  formDirty: boolean = false;
+
   constructor(
+
     public dialogRef: MatDialogRef<DetalheClienteComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private http: HttpClient,
+    private datePipe: DatePipe
+
   ) {}
+
+  ngOnInit(): void {
+
+    this.DataCadastro = this.datePipe.transform(this.data.data_cadastro, 'yyyy-MM-ddTHH:mm:ss');
+    this.UltimaAlteracao = this.datePipe.transform(this.data.ultima_alteracao, 'yyyy-MM-ddTHH:mm:ss');
+
+  }
+
+  onFieldChange() {
+    this.formDirty = true;
+  }
+
+  saveChanges() {
+
+    const apiUrl = `http://127.0.0.1:8000/pessoa/${this.data.id}/`;
+    this.data.ultima_alteracao = new Date();
+    this.http.put(apiUrl, this.data).subscribe((response: any) => {
+      console.log('Pessoa Atualizada com sucesso:', response);
+    });
+
+    this.DataCadastro = this.datePipe.transform(this.data.data_cadastro, 'yyyy-MM-ddTHH:mm:ss');
+    this.UltimaAlteracao = this.datePipe.transform(this.data.ultima_alteracao, 'yyyy-MM-ddTHH:mm:ss');
+
+  }
 }
